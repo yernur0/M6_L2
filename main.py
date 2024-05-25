d549d003423edf606074d56e1210a47c687b20bb
yernur <123850340+yernur0@users.noreply.github.com>
2024-05-25 17:21:15 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import telebot
+from config import *
+from logic import *
+
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    promt = message.text
+    api = Text2ImageAPI('https://api-key.fusionbrain.ai/', API_KEY, SECRET_KEY)
+    model_id = api.get_model()
+    uuid = api.generate("Самый огромный дом на планете", model_id)
+    images = api.check_generation(uuid)[0]
+    path = f'{message.from_user.id}.png'
+    api.get_image(images, path)
+    photo = open(path, 'rb')
+    bot.send_photo(message.chat.id, photo)
+bot.infinity_polling()
